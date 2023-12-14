4d2c406aa849609a80af0d89a70b7bf71dd059a5
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-12-14 21:36:09 +0530

    express set up

--- server.js
+++ server.js
@@ -4,3 +4,30 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
 
+// DOTENV
+dotenv.config();
+
+// REST OBJECT
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// ROUTES
+app.get("", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to full stack app ",
+  });
+});
+
+// PORT
+const PORT = process.env.PORT || 8080;
+
+// listen
+app.listen(PORT, () => {
+  console.log(`server running on  ${PORT}`.bgGreen.white);
+});
