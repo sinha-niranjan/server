d05882bf07aa9f19430ffadf3dbfd0c7fd10c995
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-12-18 11:52:26 +0530

    home route added

--- server.js
+++ server.js
@@ -23,7 +23,13 @@ app.use(morgan("dev"));
 // ROUTES
 app.use("/api/v1/auth", require("./routes/userRoutes"));
 app.use("/api/v1/post", require("./routes/postRoutes"));
-
+// Home
+app.get("/", (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: "Node server",
+  });
+});
 // PORT
 const PORT = process.env.PORT || 8080;
 
